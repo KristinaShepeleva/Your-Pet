a6291918f730b5babd5fbc56ae895ea4e5107be2
KristinaShepeleva <shepelevakristina52@gmail.com>
2023-08-21 15:53:07 +0300

    add container

--- src/components/Sharedlayout.jsx
+++ src/components/Sharedlayout.jsx
@@ -1,15 +1,18 @@
 import { Suspense } from 'react';
 import { Outlet } from 'react-router-dom';
+import Container from 'components/Container/Container';
 
 export const SharedLayout = () => {
   return (
     <>
+      <Container>
       <h2>AppBar</h2>
       <Suspense>
         <main>
           <Outlet />
         </main>
-      </Suspense>
+        </Suspense>
+        </Container>
     </>
   );
 };
