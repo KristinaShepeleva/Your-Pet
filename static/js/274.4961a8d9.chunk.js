aec2231186c3a3a27a54d5c9ccf4c13e92cba93a
KristinaShepeleva <113527872+KristinaShepeleva@users.noreply.github.com>
2023-08-21 11:30:29 +0000

    Deploying to gh-pages from @ KristinaShepeleva/your-pet@f3ea8fe6e77742fbe810a7e0613a462ecb3f6335 🚀

--- static/js/274.4961a8d9.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkyour_pet=self.webpackChunkyour_pet||[]).push([[274],{4274:function(e,u,n){n.r(u);var r=n(184);u.default=function(){return(0,r.jsx)("h2",{children:"MainPage"})}}}]);
-//# sourceMappingURL=274.4961a8d9.chunk.js.map
