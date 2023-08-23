e2db28c19c333b0719ce241f334608cffc26c14f
Kreidina <kav99mail@gmail.com>
2023-08-23 17:23:29 +0300

    опрацювання помилок

--- src/redux/auth/authFunctions.js
+++ src/redux/auth/authFunctions.js
@@ -1,42 +1,44 @@
 export const handelPending = state => {
   state.isLoading = true;
 };
+export const handelRejected = (state, { payload }) => {
+  state.isLoading = false;
+  state.error = {
+    message: payload.data.message,
+    status: payload.status,
+  };
+};
+export const registerFulfilled = (state, { payload }) => {
+  state.isLoading = false;
+  state.isNewUser = true;
+  state.isLoggedIn = true;
+  state.user = payload.user;
+  state.token = payload.token;
+  state.error = { message: '', status: null };
+};
 export const loginFulfilled = (state, { payload }) => {
   state.isLoading = false;
   state.user = payload.user;
   state.token = payload.token;
   state.isLoggedIn = true;
-  state.error = null;
+  state.error = { message: '', status: null };
   state.isNewUser = false;
-};
-export const registerFulfilled = (state, { payload }) => {
-  state.isLoading = false;
-  state.user = payload.user;
-  state.token = payload.token;
-  state.isLoggedIn = true;
-  state.error = null;
-  state.isNewUser = true;
-};
-export const handelRejected = (state, { payload }) => {
-  state.isLoading = false;
-  state.error.message = payload.data.message;
-  state.error.status = payload.status;
 };
 
 export const logoutFulfilled = state => {
   state.isLoading = false;
-  state.user = {};
-  state.token = null;
-  state.isLoggedIn = false;
-  state.error = null;
   state.isRefreshing = false;
   state.isNewUser = false;
+  state.isLoggedIn = false;
+  state.user = {};
+  state.token = null;
+  state.error = { message: '', status: null };
 };
 export const currentFulfilled = (state, { payload }) => {
   state.isLoading = false;
   state.isLoggedIn = true;
   state.isRefreshing = true;
   state.user = payload;
-  state.error = null;
+  state.error = { message: '', status: null };
   state.isNewUser = false;
 };
