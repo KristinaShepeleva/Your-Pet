18a2b2a1df1df2c30b7dcfa2e5da69626ee8d046
DenShykyta <wrkacc4@gmail.com>
2023-08-24 21:44:32 +0300

    add styles to Logout btn

--- src/components/Header/Navigation/Navigation.js
+++ src/components/Header/Navigation/Navigation.js
@@ -1,17 +1,18 @@
 import { useState } from 'react';
 import { useAuth } from '../../../hooks/useAuth';
-import {UserIcon} from "../../../helpers/icons"
+import { UserIcon } from '../../../helpers/icons';
 import MainNav from '../MainNav/MainNav';
 import UserNav from '../UserNav/UserNav';
 import AuthNav from '../AuthNav/AuthNav';
 import BurgerMenu from '../BurgerMenu/BurgerMenu';
 import { MenuIcon } from '../../../helpers/icons';
+import { LogoutUserNav } from '../UserNav/LogoutUserNav';
 import styles from './Navigation.module.css';
 
 export default function Navigation({ isDesktop, isTablet, isMobile }) {
   const [showBurgerMenu, setShowBurgerMenu] = useState(false);
   const { isLoggedIn } = useAuth();
-  const {user} = useAuth();
+  const { user } = useAuth();
 
   const toggleMobileMenu = e => {
     e.preventDefault();
@@ -28,20 +29,33 @@ export default function Navigation({ isDesktop, isTablet, isMobile }) {
       )}
       {isTablet && (
         <>
-          {isLoggedIn ? <UserNav /> : <AuthNav />}
+          {isLoggedIn ? <UserNav isTablet={isTablet} /> : <AuthNav />}
           <MenuIcon className={styles.burger} onClick={toggleMobileMenu} />
         </>
       )}
       {isMobile && (
         <>
-          {isLoggedIn && <UserNav isMobile={isMobile}/>}
+          {isLoggedIn && <UserNav isMobile={isMobile} />}
           <MenuIcon className={styles.burger} onClick={toggleMobileMenu} />
         </>
       )}
       {showBurgerMenu && (
-        <BurgerMenu onClick={toggleMobileMenu} isMobile={isMobile}>
-          {isMobile && (!isLoggedIn ? <AuthNav onClick={toggleMobileMenu} /> : <div className={styles.userData}><UserIcon  /><p>{user.name}</p></div>)}
+        <BurgerMenu
+          onClick={toggleMobileMenu}
+          isMobile={isMobile}
+          isTablet={isTablet}
+        >
+          {isMobile &&
+            (!isLoggedIn ? (
+              <AuthNav onClick={toggleMobileMenu} />
+            ) : (
+              <div className={styles.userData}>
+                <UserIcon />
+                <p>{user.name}</p>
+              </div>
+            ))}
           <MainNav onClick={toggleMobileMenu} />
+          {isMobile && <LogoutUserNav/>}
         </BurgerMenu>
       )}
     </div>
--- src/components/Header/UserNav/UserNav.js
+++ src/components/Header/UserNav/UserNav.js
@@ -4,11 +4,11 @@ import styles from './UserNav.module.css';
 import { UserIcon } from '../../../helpers/icons';
 import { LogoutUserNav } from 'components/Header/UserNav/LogoutUserNav';
 
-export default function UserNav({ isMobile, style, onClick }) {
+export default function UserNav({ isMobile, isTablet, style, onClick }) {
   const { user } = useAuth();
   return (
     <div className={styles.container} style={style}>
-      <LogoutUserNav />
+      {(!isMobile && !isTablet) && (<LogoutUserNav />)}
       <NavLink className={styles.link} onClick={onClick} to="/user">
         <UserIcon
           style={!isMobile ? { marginRight: 12 } : { marginRight: 8 }}
