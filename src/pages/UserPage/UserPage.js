e4fa47b9cbbd325b713e3c59dc019c27cf2ea843
Kreidina <kav99mail@gmail.com>
2023-08-24 16:38:40 +0300

    підключення модалок

--- src/pages/UserPage/UserPage.js
+++ src/pages/UserPage/UserPage.js
@@ -1,13 +1,34 @@
 import Container from 'components/Container/Container';
+import CongratsModal from 'components/Modals/CongratsModal/CongratsModal';
+import ModalContainer from 'components/Modals/ModalContainer/ModalContainer';
 import { UserForm } from 'components/UserForm/Form/UserForm';
+import { useState } from 'react';
+import { useSelector } from 'react-redux';
+import { selectNewUser } from 'redux/auth/authSelectors';
 
 const User = () => {
+  const isNewUser = useSelector(selectNewUser);
+
+  const [isShowModal, setIsShowModal] = useState(true);
+  const [newUser, setNewUser] = useState(isNewUser);
+  console.log(isNewUser, 'new');
+
+  const toggleModal = () => {
+    setNewUser(!newUser);
+    setIsShowModal(!isShowModal);
+  };
+
   return (
     <>
       <Container>
         <h2>UserPage</h2>
         <UserForm />
       </Container>
+      {newUser && (
+        <ModalContainer toggleModal={toggleModal}>
+          <CongratsModal toggleModal={toggleModal} />
+        </ModalContainer>
+      )}
     </>
   );
 };
