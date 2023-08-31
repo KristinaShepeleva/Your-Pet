dd3d98dd3db60feb85baf4ab67f06938bf846475
Mariia Sarbieieva <sarbeeva.m.a@gmail.com>
2023-08-31 12:27:54 +0300

    changes style

--- src/components/PetsData/PetsData.jsx
+++ src/components/PetsData/PetsData.jsx
@@ -2,12 +2,12 @@ import { PetsList } from 'components/PetsData/PetsList/PetsList';
 import css from './PetsData.module.css';
 import AddPetButton from 'components/NoticesPage/AddPetButton/AddPetButton';
 import { Link } from 'react-router-dom';
-// import { Box } from 'components/Box/Box';
+import { Box } from 'components/Box/Box';
 
 export const PetsData = () => {
   return (
-    <div className={css.wrapper}>
-      {/* <Box>  */}
+    
+     <Box>  
       <div className={css.box}>
         
         <h2 className={css.title}>My pets:</h2>
@@ -17,7 +17,7 @@ export const PetsData = () => {
       </div>
 
         <PetsList />
-        {/* </Box> */}
-    </div>
+        </Box> 
+   
   );
 };
