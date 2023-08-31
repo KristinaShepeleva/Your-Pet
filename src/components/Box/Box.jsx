f79f4f43b342489cefcf97755895003cb6468ecc
Mariia Sarbieieva <sarbeeva.m.a@gmail.com>
2023-08-31 13:58:35 +0300

    changes style and updateUserAvatar

--- src/components/Box/Box.jsx
+++ src/components/Box/Box.jsx
@@ -2,7 +2,7 @@ import PropTypes from 'prop-types';
 import css from './Box.module.css'
 export const Box = ({children}) => {
     return (
-        <div className={css.box}>{children}</div>
+        <div className={css.boxPetData}>{children}</div>
     )
 }
 
